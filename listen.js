ab613df773186cf850c37a564ef68ac0f29ab974
PeteHailwood <peter.hailwood@hotmail.co.uk>
2021-11-04 14:33:49 +0000

    edit listen.js to get heroku to work

--- listen.js
+++ listen.js
@@ -1,7 +1,4 @@
 const { PORT = 9090 } = process.env;
-
-const express = require("express");
-const app = express();
-app.use(express.json());
+const app = require("./app.js")
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
